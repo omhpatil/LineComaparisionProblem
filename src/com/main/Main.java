759f3e5e5647444abb842b87ec9cdc2626c48c75
omhpatil <patilom887@gmail.com>
2024-11-25 08:28:43 +0530

    UC1 updated

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,7 +1,27 @@
 package com.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
+        Scanner scanner = new Scanner(System.in);
+
+        // Input coordinates for the first point
+        System.out.println("Enter coordinates of the first point x1: ");
+        double x1 = scanner.nextDouble();
+        System.out.println("Enter coordinates of the first point y1: ");
+        double y1 = scanner.nextDouble();
+
+        // Input coordinates for the second point
+        System.out.println("Enter coordinates of the second point y2: ");
+        double x2 = scanner.nextDouble();
+        System.out.println("Enter coordinates of the second point y1: ");
+        double y2 = scanner.nextDouble();
+
+        // Calculate the length of the line
+        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("The length of the line is: " + length);
+
+        scanner.close();
     }
 }
